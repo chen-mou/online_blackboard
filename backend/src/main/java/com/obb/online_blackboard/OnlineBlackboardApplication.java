8371f0be6f38fe0be429424ecaac978d6f119d2b
ChenZheng <1007324849@qq.com>
2022-10-27 19:31:14 +0800

    配置包扫描路径，改坐标属性名

--- backend/src/main/java/com/obb/online_blackboard/OnlineBlackboardApplication.java
+++ backend/src/main/java/com/obb/online_blackboard/OnlineBlackboardApplication.java
@@ -2,8 +2,9 @@ package com.obb.online_blackboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"tool", "com.obb.online_blackboard"})
 public class OnlineBlackboardApplication {
 
     public static void main(String[] args) {
