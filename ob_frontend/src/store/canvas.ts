bda055b919d6e1aa63102b43bd3880d6587d2fda
ChenZheng <1007324849@qq.com>
2022-10-28 17:28:33 +0800

    feat: canvas相关

---
+++ ob_frontend/src/store/canvas.ts
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia";
+
+export const useCanvasStore = defineStore('canvas', {
+  state: () => ({}),
+  actions: {},
+  getters: {},
+})
--- ob_frontend/src/views/canvas/index.vue
+++ ob_frontend/src/views/canvas/index.vue
@@ -1,13 +1,32 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useUserStore } from "@/store/user";
 
 export default defineComponent({
   name: "BlackboardCanvas",
+  setup() {
+    const userStore = useUserStore()
+    return {
+      userStore
+    }
+  },
+  watch: {
+    'userStore.hasLogin'(newVal) {
+      console.log(newVal)
+      if (newVal) {
+        return
+      }
+      //todo 登出处理
+      return
+    }
+  }
 })
 </script>
 
 <template>
-  <div>canvas</div>
+  <div>
+    canvas
+  </div>
 </template>
 
 <style scoped>
