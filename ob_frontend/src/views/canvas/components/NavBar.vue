a6f168cdf9d32dba29242e48cf64d0491d72a669
Tomao <2694235519@qq.com>
2022-10-29 13:02:40 +0800

    feat: 新增navBar

---
+++ ob_frontend/src/views/canvas/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name:"NavBar"
+})
+
+</script>
+
+
+<style>
+ 
+
+</style>
