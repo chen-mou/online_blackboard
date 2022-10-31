b7536aa58ea64a1e8ddbba5910983c35d412ceb1
Tomao <2694235519@qq.com>
2022-10-31 21:34:42 +0800

    feat: 新增导出功能

--- ob_frontend/src/views/canvas/components/operation.vue
+++ ob_frontend/src/views/canvas/components/operation.vue
@@ -3,10 +3,17 @@
         <span @click="cleanCanvas">
             清空
         </span>
+        <span>
+            回退
+        </span>
+        <a  @click="exportAsPng">
+            导出
+        </a>
     </div>
 </template>
 
 <script lang="ts" setup>
+
 import Canvas from "@/utils/Canvas/canvas";
 import {inject, nextTick, onMounted, reactive, ref} from "vue"
 let canvas =ref()
@@ -17,8 +24,9 @@ canvas=canvasInjetct
     console.log(canvas.value)
 })
 
-
-
+/**
+ * 清空Canvas
+ */
 const cleanCanvas=()=>{
     if(canvas.value){
         canvas.value.data=[]
@@ -28,8 +36,19 @@ const cleanCanvas=()=>{
     }
     }
 
-
-
+/**
+ * 导出图片
+ */
+const exportAsPng=()=>{
+   const el = document.createElement('a');
+  // 设置 href 为图片经过 base64 编码后的字符串，默认为 png 格式
+  el.href = (canvas.value as Canvas) .canvas.toDataURL()
+  el.download = '项目名称';
+  
+  // 创建一个点击事件并对 a 标签进行触发
+  const event = new MouseEvent('click');
+  el.dispatchEvent(event);
+}
 
 
 
